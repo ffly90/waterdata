5def03c3ddce6c022965120720a80cb7151d83f0
margau <dev@marvingaube.de>
2018-08-10 08:59:18 +0200

    Simple save

---
+++ data_in/src/main/java/web/Data.java
@@ -0,0 +1,43 @@
+package web;
+
+import org.springframework.data.annotation.Id;
+
+
+public class Data {
+    @Id private String id;
+    private String sensor;
+
+    private double value;
+    private long timestamp;
+    public String measureID;
+
+
+    public void setTimestamp(){
+        timestamp = System.currentTimeMillis() / 1000L;
+    }
+    public long getTimestamp(){
+        return timestamp;
+    }
+    public String getSensor() {
+        return sensor;
+    }
+    public void setSensor(String sensor) {
+        this.sensor = sensor;
+    }
+    public double getValue() {
+        return value;
+    }
+    public void setValue(double value) {
+        this.value = value;
+    }
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id='" + id + '\'' +
+                ", sensor='" + sensor + '\'' +
+                ", value=" + value +
+                ", timestamp=" + timestamp +
+                ", measureID='" + measureID + '\'' +
+                '}';
+    }
+}
